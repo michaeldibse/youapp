411a3a9a722c8a11c7f04305c177dc329d84d3fb
Michael Karbasch <michael.karbasch@icloud.com>
2020-09-05 16:33:41 +0200

    modified gitignore to stop tracking constants.swift, added Constants.swift

--- youapp/Constants.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Constants.swift
-//  youapp
-//
-//  Created by Michael Karbasch on 30.08.20.
-//  Copyright © 2020 Michael Karbasch. All rights reserved.
-//
-
-import Foundation
---
+++ youapp/ConstantsTemplate.swift
@@ -0,0 +1,30 @@
+//
+//  ConstantsTemplate.swift
+//  youapp
+//
+//  Created by Michael Karbasch on 05.09.20.
+//  Copyright © 2020 Michael Karbasch. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 
+ Add your own key and uncomment the code
+ 
+ */
+
+
+/*
+ 
+struct Constants {
+    
+    // API Constants
+    
+    static var API_KEY = ""
+    static var API_URL = ""
+    static var API_PLAYLISTID = "'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.API_URL)&key=\(Constants.API_KEY)"
+}
+ 
+*/
+
